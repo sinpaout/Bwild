697c900a0a86076faa8a67d562a1ae116df2f900
Tatsuhiko Seki <osekiharn@gmail.com>
2018-07-24 22:17:52 +0900

    fix pathMap

--- next.config.js
+++ next.config.js
@@ -2,25 +2,16 @@ const SUMMARY_JSON = require('./json/summary.json')
 
 module.exports = {
   exportPathMap: function () {
-    const pathMap = SUMMARY_JSON.fileMap && Object.keys(SUMMARY_JSON.fileMap).map((path) => {
-      const { base, souceBase } = SUMMARY_JSON.fileMap[path];
-      return {
-        [`p/${souceBase}`] : { page: '/post', query: { path: [`${base}`] } }
-      }
+    let pathMap = {}
+    SUMMARY_JSON.fileMap && Object.keys(SUMMARY_JSON.fileMap).forEach((path) => {
+      const { base, sourceBase } = SUMMARY_JSON.fileMap[path];
+      pathMap[`/p/${sourceBase}`] = { page: '/post', query: { path: base } }
     })
+   
     return {
       '/': { page: '/' },
-      'about': { page: 'about' },
+      '/about': { page: '/about' },
       ...pathMap
     }
-  },
-  module: {
-    loaders: [
-      { test: /\.json$/, loaders: ['json-loader',  'yaml-frontmatter-loader']},
-      {
-        test: /\.md$/,
-        use: [ 'json-loader', 'yaml-frontmatter-loader' ]
-      }
-    ]
   }
 }
